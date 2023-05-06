3e8aca59b08c0a6b1ede33a2650d8feae0e14cde
skywalk94 <skywalk94@foxmail.com>
2023-05-06 15:31:12 +0800

    init

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import 'react-native-gesture-handler'
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import StackNavigator from "./src/router/StackNavigator"
-const App = () => {
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function App() {
   return (
-    <NavigationContainer>
-      <StackNavigator />
-    </NavigationContainer>
-  );
+    <View style={styles.container}>
+      <Text>明知山</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
--- README.md
+++
@@ -1,8 +0,0 @@
-### react-native练习项目
-
-```
-yarn
-
-yarn android
-```
-教程地址：https://blog.csdn.net/ak852369/category_11150157.html 
