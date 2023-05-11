991205e249adf638e57f6ee61100e4dd7ad2e31c
skywalk94 <skywalk94@foxmail.com>
2023-05-11 14:58:48 +0800

    引入native-base

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
 import AppNavigation from "./src/navigation/appNavigation"
+import { extendTheme, NativeBaseProvider } from "native-base"
 
 export default () => {
+  const theme = extendTheme({
+    colors: {
+      primary: {
+        50: '#6fc6f3', 100: '#5dbff2', 200: '#4bb8f0', 300: '#39b0ef', 400: '#27a9ed',
+        500: '#15a2ec', 600: '#1296db', 700: '#118ac9', 800: '#0f7db7', 900: '#0e71a5',
+      }
+    }
+  })
+
   return (
-    <AppNavigation />
+    <NativeBaseProvider theme={theme}>
+      <AppNavigation />
+    </NativeBaseProvider>
   )
 }
--- src/screens/Index.jsx
+++ src/screens/Index.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-import { Button, Text } from 'react-native'
+import { Text } from 'react-native'
 import PopUp from "../components/PopUp"
+import { Button } from "native-base"
 
 export default () => {
     const [visible, setVisible] = useState(false)
 
     return (
         <>
-            <Button title="打开弹窗" onPress={() => setVisible(true)} />
+            <Button onPress={() => setVisible(true)}>打开弹窗</Button>
             <PopUp visible={visible} close={() => setVisible(false)}>
                 <Text>占位</Text>
             </PopUp>
