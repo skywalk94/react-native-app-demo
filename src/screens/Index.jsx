309d7fb899dc24c9777e2991c6d7d886500d7b8e
skywalk94 <skywalk94@foxmail.com>
2023-05-11 11:02:00 +0800

    删除地址选择

--- src/screens/Index.jsx
+++ src/screens/Index.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import { Button, Text } from 'react-native'
-import CitySelect from "../components/CitySelect"
+import PopUp from "../components/PopUp"
 
 export default () => {
+    const [visible, setVisible] = useState(false)
 
     return (
         <>
-            <CitySelect />
+            <Button title="打开弹窗" onPress={() => setVisible(true)} />
+            <PopUp visible={visible} close={() => setVisible(false)}>
+                <Text>占位</Text>
+            </PopUp>
         </>
     )
 }
