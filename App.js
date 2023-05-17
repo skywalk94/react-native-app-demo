942efa74baaa4fc8e00944903a42d5a99515acf5
skywalk94 <skywalk94@foxmail.com>
2023-05-17 10:12:18 +0800

    因NativeBase有大量警告，移除该依赖

--- App.js
+++ App.js
@@ -1,19 +1,8 @@
 import AppNavigation from "./src/navigation/appNavigation"
-import { extendTheme, NativeBaseProvider } from "native-base"
 
 export default () => {
-  const theme = extendTheme({
-    colors: {
-      primary: {
-        50: '#6fc6f3', 100: '#5dbff2', 200: '#4bb8f0', 300: '#39b0ef', 400: '#27a9ed',
-        500: '#15a2ec', 600: '#1296db', 700: '#118ac9', 800: '#0f7db7', 900: '#0e71a5',
-      }
-    }
-  })
 
   return (
-    <NativeBaseProvider theme={theme}>
-      <AppNavigation />
-    </NativeBaseProvider>
+    <AppNavigation />
   )
 }
--- src/screens/Index.jsx
+++ src/screens/Index.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
-import { Text } from 'react-native'
+import { Text, Button } from 'react-native'
 import PopUp from "../components/PopUp"
-import { Button } from "native-base"
 
 export default () => {
     const [visible, setVisible] = useState(false)
 
     return (
         <>
-            <Button onPress={() => setVisible(true)}>打开弹窗</Button>
+            <Button title='打开弹窗' onPress={() => setVisible(true)} />
             <PopUp visible={visible} close={() => setVisible(false)}>
+                <Text>占位</Text>
+                <Text>占位</Text>
                 <Text>占位</Text>
             </PopUp>
         </>
